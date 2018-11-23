9999754ba353d786ceac74136844743d5e15d3a4
Alexandre 'Kidev' Poumaroux <poumar_a@epita.fr>
2018-11-23 04:58:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AdaProject
+
+This is a simple 2D physics game for STM32F429
+Made with the help of AdaCore's Ada_Drivers_Library
---
+++ src/AdaPhysics2D.adb
@@ -0,0 +1,112 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                        Copyright (C) 2018, Kidev                         --
+--                                                                          --
+--  Redistribution and use in source and binary forms, with or without      --
+--  modification, are permitted provided that the following conditions are  --
+--  met:                                                                    --
+--     1. Redistributions of source code must retain the above copyright    --
+--        notice, this list of conditions and the following disclaimer.     --
+--     2. Redistributions in binary form must reproduce the above copyright --
+--        notice, this list of conditions and the following disclaimer in   --
+--        the documentation and/or other materials provided with the        --
+--        distribution.                                                     --
+--     3. Neither the name of the copyright holder nor the names of its     --
+--        contributors may be used to endorse or promote products derived   --
+--        from this software without specific prior written permission.     --
+--                                                                          --
+--   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    --
+--   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      --
+--   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  --
+--   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   --
+--   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, --
+--   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       --
+--   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  --
+--   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  --
+--   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    --
+--   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  --
+--   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Last_Chance_Handler;  pragma Unreferenced (Last_Chance_Handler);
+--  The "last chance handler" is the user-defined routine that is called when
+--  an exception is propagated. We need it in the executable, therefore it
+--  must be somewhere in the closure of the context clauses.
+
+with STM32.Board; use STM32.Board;
+with HAL.Bitmap; use HAL.Bitmap;
+with HAL.Framebuffer; use HAL.Framebuffer;
+with Ada.Real_Time; use Ada.Real_Time;
+--with HAL.Touch_Panel;       use HAL.Touch_Panel;
+--with STM32.User_Button;     use STM32;
+with BMP_Fonts;
+with LCD_Std_Out;
+
+with Circles;
+with Worlds;
+with Vectors2D; use Vectors2D;
+
+procedure AdaPhysics2D is
+
+   BG : constant Bitmap_Color := (Alpha => 255, others => 0);
+   procedure Init;
+   procedure Clear;
+
+   procedure Init is
+   begin
+      Display.Initialize;
+      Display.Initialize_Layer(1, ARGB_8888);
+      LCD_Std_Out.Set_Orientation(Landscape);
+      LCD_Std_Out.Set_Font(BMP_Fonts.Font12x12);
+      LCD_Std_Out.Current_Background_Color := BG;
+      Clear;
+   end Init;
+
+   procedure Clear is
+   begin
+      Display.Hidden_Buffer(1).Set_Source(BG);
+      Display.Hidden_Buffer(1).Fill;
+      LCD_Std_Out.Clear_Screen;
+      Display.Update_Layer(1, Copy_Back => True);
+   end Clear;
+
+   Count : Integer := 0;
+   Period : constant Time_Span := Milliseconds(1000);
+   NextTick : Time := Clock;
+
+   C1 : Circles.CircleAcc;
+   C2 : Circles.CircleAcc;
+   W1 : Worlds.World;
+   VecZero : Vec2D;
+   Vec1, Vec2, Grav : Vec2D;
+
+   fps : constant Float := 26.0;
+   dt : constant Float := 1.0 / fps;
+
+begin
+   VecZero := Vec2D'(x => 0.0, y => 0.0);
+   Vec1 := Vec2D'(x => 1.0, y => 2.0);
+   Vec2 := Vec2D'(x => 3.0, y => 2.0);
+   Grav := Vec2D'(x => 8.0, y => 0.0);
+
+   W1.Init(dt);
+
+   C1 := Circles.Create(Vec1, VecZero, Grav, 10.0, 0.9, 1.0);
+   C2 := Circles.Create(Vec2, VecZero, Grav, 5.0, 0.9, 2.0);
+
+   W1.Add(C1);
+   W1.Add(C2);
+
+   Init;
+   loop
+      Clear;
+
+      W1.Step;
+
+      Count := Count + 1;
+      -- NextTick := NextTick + Period;
+      -- delay until NextTick;
+   end loop;
+
+end AdaPhysics2D;
